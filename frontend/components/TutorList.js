d5d0ad910578d4d5a136f65d23eddb6e3bdb63d8
Conrado <conradob@live.com>
2019-05-13 21:59:06 +0100

    Displaying the proper Tutor with Description

--- frontend/components/TutorList.js
+++ frontend/components/TutorList.js
@@ -1,10 +1,78 @@
+import Link from 'next/link';
+import styled from 'styled-components';
+
+const TutorUl = styled.ul`
+  display: grid;
+  grid-gap: 4rem;
+  grid-template-columns: repeat(auto-fit, minmax(40%, 1fr));
+`;
+
+const TutorItemStyle = styled.li`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  box-shadow: ${props => props.theme.bs};
+  list-style: none;
+  padding: 2rem;
+  border: 1px solid ${props => props.theme.offWhite};
+  h2 {
+    border-bottom: 2px solid red;
+    margin-top: 0;
+    margin-bottom: 2rem;
+    padding-bottom: 2rem;
+  }
+  .tutor-meta {
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(20px, 1fr));
+    grid-gap: 1rem;
+    text-align: center;
+    & > * {
+      margin: 0;
+      background: rgba(0, 0, 0, 0.03);
+    }
+    small {
+      display: block;
+      margin-bottom: 1rem;
+    }
+  }
+`;
+
+const BigButton = styled.button`
+  font-size: 3rem;
+  background: none;
+  border: 0;
+  &:hover {
+    color: ${props => props.theme.red};
+    cursor: pointer;
+  }
+`;
+
 const TutorList = ({ list = [] }) => {
   return (
-    <>
-      {list.map(el => (
-        <span key={el.id}>{el.name}</span>
-      ))}
-    </>
+    <div>
+      <h2>
+        You have {list.length} tutor{list.length > 1 ? 's' : ''}.
+      </h2>
+      <TutorUl>
+        {list.map(tutor => (
+          <TutorItemStyle key={tutor.id}>
+            <Link
+              href={{
+                pathname: '/tutor',
+                query: { id: tutor.id },
+              }}
+            >
+              <a>
+                <div className="order-meta">
+                  <p>{tutor.name}</p>
+                  <small>{tutor.description}</small>
+                </div>
+              </a>
+            </Link>
+            <BigButton>&times;</BigButton>
+          </TutorItemStyle>
+        ))}
+      </TutorUl>
+    </div>
   );
 };
 
